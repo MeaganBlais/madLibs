9e6255c48426c8e9ed3be2622544bc2ca543d4c9
Meagan Blais <meaganblais@gmail.com>
2017-11-14 15:47:31 -0500

    add input field and collect responses

--- madLibs.js
+++ madLibs.js
@@ -4,16 +4,24 @@ var prompts = [
   'Type a noun'
 ];
 
+// catch the answers in an array
+var answers = [];
+
 var currentPrompt = 0;
 
 var nextPrompt = function() {
+  // push all answers expect initial blank input
+  if (currentPrompt != 0) {
+    answers.push($('input').val());
+  }		
   if (currentPrompt < prompts.length) {
     //put first prompt in all html elements with class
-    $('.prompt').html(prompts[currentPrompt]);
+		$('.prompt').html(prompts[currentPrompt] + '<br><input type="text">');
     // move the next prompt into variable currentPrompt
     currentPrompt = currentPrompt + 1;  
   } else {
     $('.prompt').html("that's all for now!");
+    alert(answers);
   }
 }
 
